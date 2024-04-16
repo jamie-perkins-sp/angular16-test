0b44a552a21c945db0619da69d5c66bc41f1283b
Jamie Perkins <jamie.perkins@sailpoint.com>
2024-04-16 13:39:11 -0600

    takeUntilDestroyed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular16-test';
+
+  subject = new Subject();
+
+  ngOnInit() {
+    this.subject.pipe(
+      takeUntilDestroyed()
+    ).subscribe(console.log)
+  }
 }
